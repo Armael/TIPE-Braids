2ec864487148939f6eb6ea7266170e72f2283b11
Armael <mail.armael.gueneau@gmail.com>
2011-12-02 16:58:51 +0100

    Ajout de make_braid

--- Braid.ml
+++ Braid.ml
@@ -3,3 +3,8 @@ type braid = {word : int list; size : int}
 let inv braid = {word = List.map (fun x -> -x) (List.rev braid.word); size = braid.size}
 
 let concat b1 b2 = {word = b1.word @ b2.word; size = max b1.size b2.size}
+
+let make_braid ?(size = -1) w =
+    let s = (if size = -1 then 1 + List.fold_left (fun a x -> max a (abs x)) 0 w
+                          else size) in
+    {word = w; size = s};;
--- Braid.mli
+++ Braid.mli
@@ -1,3 +1,4 @@
 type braid = {word : int list; size : int};;
 val inv : braid -> braid;;
 val concat : braid -> braid -> braid;;
+val make_braid : ?size:int -> int list -> braid;;
