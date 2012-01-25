c65abda5f53c2bee09762c5cb25ff5d14e46bcfa
Armael <mail.armael.gueneau@gmail.com>
2012-01-25 19:05:40 +0100

    Petite application : Diffie-Hellman

---
+++ DiffieHellman.ml
@@ -0,0 +1,25 @@
+open Canonical;;
+open Braid;;
+
+let n = 80 and l = 12;;
+
+let make_pubkey () = canonicize (random_braid_sequence n l);;
+
+let make_alice_privkey () = canonical_form (random_LBn_braid n (5*l));;
+let make_bob_privkey () = canonical_form (random_UBn_braid n (5*l));;
+
+let make_exchange_key pub priv = conjugate pub priv;;
+
+let make_key exch_key priv = conjugate exch_key priv;;
+
+let () = 
+    let public = make_pubkey () in
+    let alice_priv = make_alice_privkey () in
+    let bob_priv = make_bob_privkey () in
+    let alice_exch = make_exchange_key public alice_priv in
+    let bob_exch = make_exchange_key public bob_priv in
+    
+    let tA = make_key bob_exch alice_priv in
+    let tB = make_key alice_exch bob_priv in
+
+    if compare_permlists tA tB then print_string "Ok" else print_string "Pas ok";;
