89c0cbbf82f9eb5d47866d23cf0e21eeb2812032
Armael <mail.armael.gueneau@gmail.com>
2012-03-03 02:37:03 +0100

    Ajout de nombreux commentaires pour les fonctions de Canonical.ml, dans le style de ocamlweb, avec formules en LaTeX

--- Canonical.ml
+++ Canonical.ml
@@ -1,15 +1,41 @@
+
+(*p
+\usepackage[T1]{fontenc}
+\usepackage{amsmath}
+\usepackage{amsfonts}
+\usepackage{amssymb}
+*)
+
 module P = Permutation;;
+
+(* Type décrivant une tresse représentée sous la forme $\Delta^r A_1...A_n$ où
+   $A_1,...,A_n$ sont des tresses simples. Les tresses simples sont ici décrites
+   par leurs permutations associées.
+*)
 
 type braid_permlist = {delta_power : int; permlist : P.permutation list};;
 
-(* Convert a braid word to a permlist *)
+(*s Conversion d'une tresse décrit par un mot sur l'alphabet des générateurs
+   en une représentation sous forme de liste de permutations.
+   
+   Algorithme : Si l'on rencontre un $\sigma _i$ alors on ajoute à la liste des
+   permutations la transposition correspondante.
+   Si l'on rencontre un ${\sigma _i}^{-1}$, on utilise le fait que
+   ${\sigma _i}^{-1} = \Delta^{-1} \Delta {\sigma _i}^{-1}$ et que $\Delta {\sigma _i}^{-1}$
+   est une tresse positive. On l'ajoute à la liste et on fait remonter le $\Delta^{-1}$
+   en composant par $\tau$ .
+   
+   On simplifie les calculs en se rappelant que $\tau^2 = id$.
+*)
+
 let get_permlist_decomposition (b : Braid.braid) = 
     let n = b.Braid.size in
-    (* Calcule Delta * sigma^-1 que l'on sait être un facteur canonique *)
+    (* Calcule $\Delta \times \sigma^{-1}$ que l'on sait être un facteur canonique *)
     let neg_to_perm x =
         let perm = P.make_delta n in
-        P.transpose perm (-x-1) (-x); (* x négatif *)
+        P.transpose perm (-x-1) (-x); (* $x$ négatif *)
         perm in
+    (* Applique l'algorithme, en mémorisant la puissance de $\tau$ à appliquer *)
     let (delta_pow, perm_stack) =
         List.fold_left (fun (delta_pow, perm_stack) x ->
                           if x > 0 then
@@ -20,6 +46,7 @@ let get_permlist_decomposition (b : Braid.braid) =
                              (neg_to_perm x, 0)::
                              (List.map (fun (p, tau_pow) -> (p, tau_pow + 1)) perm_stack)))
                        (0, []) b.Braid.word in
+    (* On applique la puissance de $\tau$ si nécessaire *)
     let perm_list =
         List.rev_map (fun (perm, tau_pow) -> if (tau_pow mod 2) = 1
                                                 then P.tau perm
@@ -27,7 +54,15 @@ let get_permlist_decomposition (b : Braid.braid) =
                      perm_stack in
     {delta_power = delta_pow; permlist = perm_list};;
 
-(* Conversion functions to left-weighted/canonical/normal form *)
+(*s Mise sous forme maximale à gauche (left-weighted) d'une liste de permutations (et non
+   d'une tresse complète : voir canonicize pour celà).
+   
+   Algorithme : On considère tour à tour des couples de tresses simples. On calcule alors
+   le «finishing set» de la première et le «starting set» de la seconde. On fait la 
+   différence ensembliste entre le «starting set» et le «finishing set» et l'on ajoute
+   les éléments de la différence à la première tresse, et on les retire de la deuxième
+   (concrètement, une composition de permutations).
+*)
 
 let make_left_weighted start_pl =
   let continue = ref true in (* référence mis à true à chaque fois que iter provoque une
@@ -60,6 +95,13 @@ let make_left_weighted start_pl =
   done;
   !current_pl;;
 
+(*s Met une tresse décrite sous forme de liste de permutations sous forme canonique.
+   
+   Algorithme : Rend la liste de tresses simples maximale à gauche, et collecte
+   les tresses simples égales à $\Delta$ en tête de liste (s'il y a un $\Delta$, il
+   est forcément en tête de liste). Incrémente le champ delta\_power en conséquence.
+*)
+   
 let canonicize bpl =
   let dp = ref bpl.delta_power 
   and pl = ref (make_left_weighted bpl.permlist)
@@ -74,13 +116,27 @@ let canonicize bpl =
   done;
   {delta_power = !dp; permlist = !pl};;
 
+(* Met une tresse décrite par un mot de tresse sous forme canonique.
+*)
+
 let canonical_form b = canonicize (get_permlist_decomposition b);;
 
-(* Equality tests *)
+(*s Tests d'égalité.
+
+   Revient à mettre sous forme canonique, et les comparer.
+*)
+
 let compare_braids b1 b2 = (canonical_form b1) = (canonical_form b2);;
 let compare_permlists bpl1 bpl2 = (canonicize bpl1) = (canonicize bpl2);;
 
-(* Algebraic operations on permlists *)
+(*s Opérations algébriques sur les tresses sous forme de listes de permutations.
+*)
+
+(* Produit.
+
+   Formule : $(\Delta^p A_1...A_l)(\Delta^q B_1...B_l') =
+               \Delta^{p+q} \tau^q(A_1)...\tau^q(A_l) B_1...B_l$
+*)
 
 let product a b =
   {delta_power = a.delta_power + b.delta_power;
@@ -89,6 +145,14 @@ let product a b =
 
 let (<*>) = product;;
 
+
+(* Inverse.
+
+   Formule : $(\Delta^r A_1...A_l)^{-1} =
+               \Delta^{-r-l} \tau^{-r-l}(A'_l)...\tau^{-r-1}(A'_1)$
+   avec $A'_i = A_i^{-1} \Delta$ en voyant $\Delta$ et $A_i$ en tant que permutations (??)
+*)
+   
 let inverse b =
   let l = List.length b.permlist and q = b.delta_power in
   let (_, pl') =
@@ -102,10 +166,17 @@ let inverse b =
     permlist = pl'
   };;
 
+(* Conjugué.
+
+   Conjugué de A par B : $B A B^{-1}$
+*)
 
 let conjugate a b = b <*> a <*> (inverse b);;
 
-(* Appeler avec l = DOUZE, n de l'ordre de 10^(1 ou 2) *)
+(*s Génère une tresse sous forme de liste de permutations aléatoire.
+   À appeler avec l = DOUZE, n de l'ordre de 80 (en gros, $10^{1\ ou \ 2}$)
+*)
+
 let random_braid_sequence n l =
   let rec loop acc = function
     | 0 -> acc
@@ -113,6 +184,9 @@ let random_braid_sequence n l =
   in
   {delta_power = l-(Random.int (3*l)); (* tout à fait arbitraire *)
    permlist = loop [] l};;
+
+(*s Affiche une tresse sous forme de permlist : (delta\_power | Permutations)
+*)
 
 let print_braid_permlist bpl =
   Printf.printf "(%d | " bpl.delta_power;
--- README.markdown
+++ README.markdown
@@ -7,3 +7,11 @@ Pour tester, il y a un petit programme simulant un protocole d'échange de clé
 inspiré de Diffie-Hellman employant la conjugaison par des tresses commutant entre elles.
 
 Compilation : ocamlbuild DiffieHellman.byte
+
+
+Il est possible d'exporter le code source grâce à ocamlweb. Pour ce faire, pour convertir
+en latex le code du fichier Canonical.ml par exemple :
+
+    ocamlweb Canonical.ml -o Canonical.tex
+    sed -i 's/latin1/utf8/' Canonical.tex # Workaround au sujet de l'encodage en attendant
+                                          # une meilleure solution
