c0cc237c8dc79bccd5183f0215e2c6c7de2291af
Armael <mail.armael.gueneau@gmail.com>
2012-03-03 01:17:48 +0100

    Code de test/debug plus précis

--- test.ml
+++ test.ml
@@ -1,4 +1,27 @@
 open Braid;;
-Braid_Print.display {word = [4;3;2;-1]; size = 5};;
-Graphics.read_key ();;
-Graphics.close_graph ();;
+open Canonical;;
+
+let print_int_list = List.iter (fun x -> Printf.printf "%d " x);;
+
+let print_int_array_list = List.iter
+    (fun a -> Printf.printf "[ ";
+              Array.iter (fun x -> Printf.printf "%d " x) a;
+              Printf.printf " ] ");; 
+
+let _ =
+(*    let b = random_braid_sequence 12 80 in
+    let bc = canonicize b in 
+    let i = inverse b in
+    let p = b <*> i in
+    let pc = canonicize p in
+    print_braid_permlist pc *)
+
+
+    let b = random_braid 80 12 in
+    let i = inv b in
+    let p = i ++ b in
+    let pc = canonical_form p in
+    print_string (if Braid_Reverse.is_nil p then "OK" else "Pas ok");
+    print_newline ();
+
+    print_braid_permlist pc;;
