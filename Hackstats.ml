7bdeb028e2f73907b5b322e0b22d401e3d6877a3
Armael <mail.armael.gueneau@gmail.com>
2012-06-08 19:35:31 +0200

    Programme tentant l'algorithme de recherche de conjugateur sur de nombreuses tresses aléatoires, pour différentes tailles (version propre)

--- Hackstats.ml
+++ Hackstats.ml
@@ -1,43 +1,35 @@
 open Braid;;
 open Canonical;;
 open Hack;;
+open Sys;;
 
-(*
-let n = 80 and l = 12;;
-*)
-
-let n = 20 and l = 10;;
+let patterns = [|(45, 3); (50, 5); (70, 7); (90, 12)|];;
 
 let put_delta d b = {bpl_size = b.bpl_size; delta_power = d; permlist = b.permlist};;
 
-let test () = 
-  let pub =  (canonical_form (random_braid n l)) in
-  let conjugateur =  (canonical_form (random_braid n l)) in
+let test n l = 
+  Printf.printf "%d,%d: " n l;
+  
+  let pub =  (canonicize (random_braid_sequence n l)) in
+  let conjugateur =  (canonicize (random_braid_sequence n l)) in
   let conjugue = canonicize (conjugate pub conjugateur) in
-
-  Printf.printf "Public : ";
-  print_braid_permlist pub;
-  Printf.printf "\nConjugué : ";
-  print_braid_permlist conjugue;
-  Printf.printf "\nConjugateur : ";
-  print_braid_permlist conjugateur;
-  Printf.printf "\n";
-  flush stdout;
 
   try let guessed_priv = try_hack pub conjugue in
       if permlists_equal guessed_priv conjugateur then (
-	Printf.printf "\nClef devinée : ";
-	print_braid_permlist (canonicize guessed_priv);
-	Printf.printf "\n"
+        	Printf.printf "1\n"; (* Clé devinée exactement *)
       ) else (
-	Printf.printf "Mauvaise clef\n"
+        	Printf.printf "2\n"; (* Clé devinée à Delta² près *)
       )
   with
-      Failure s -> Printf.printf "Échec de l'attaque";
-
-  Printf.printf "\n\n";;
+      Failure s -> Printf.printf "0\n"; (* Clé non trouvée *)
+      flush stdout;;
 
 let () = 
-  for i = 0 to 100 do
-    test ()
+  let nb = (if Array.length argv = 1 then 100
+           else int_of_string argv.(1)) in
+  for i = 0 to Array.length patterns - 1 do
+      let (n, l) = patterns.(i) in
+          for j = 1 to nb do
+            test n l
+          done
   done
